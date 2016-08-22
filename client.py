396db138a1f5c76799a922f4881e33fd1d8d9aed
ianl <ianl@gofreewire.com>
2016-08-22 14:46:01 -0700

    Setting up RSA-based host key checking

--- client.py
+++ client.py
@@ -3,24 +3,79 @@ from sys import argv as args
 from sys import stdin
 from threading import Thread
 import select
+from os.path import isfile
+from Crypto.PublicKey import RSA
 
 host = args[1]
 port = int(args[2])
-size = 1024
+size = 2048
+
+# import/generate host RSA keys
+if isfile('client_hostkey') and isfile('client_hostkey.pub'):
+    privkeyString = file('client_hostkey', 'r').read()
+    pubkeyString = file('client_hostkey.pub', 'r').read()
+    privkey = RSA.importKey(privkeyString)
+    pubkey = RSA.importKey(pubkeyString)
+else:
+    print 'Generating new host RSA keys...'
+    privkey = RSA.generate(16384)
+    pubkey = privkey.publickey()
+    # write them to files for future use
+    privkeyString = privkey.exportKey()
+    pubkeyString = pubkey.exportKey()
+    privkeyFile = file('client_hostkey', 'w')
+    pubkeyFile = file('client_hostkey.pub', 'w')
+    privkeyFile.write(privkeyString)
+    pubkeyFile.write(pubkeyString)
+    privkeyFile.close()
+    pubkeyFile.close()
+
+print 'Connecting to ' + host + ':' + str(port)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host,port))
 input = [s, stdin]
 running = True
+
+# Connection Stages:
+# 0 - Initial TCP connection established
+# 1 - Banners exchanged
+# 2 - Server public RSA key received, client public key sent
+# 3 - Server sends client public key back, encrypted with client's public key
+# 4 - Encryption negotiated, connection fully established.
+connectionStage = 0
+
 while running:
     inputready,outputready,exceptready = select.select(input, [], [])
     for i in inputready:
         if i == s:
             data = s.recv(size).strip()
             if data:
-                print 'SERVER: ' + data
-                # server acknowledging our disconnect
-                if data == 'BYE':
-                    running = False
+                if connectionStage == 0:
+                    if data == 'BANNER':
+                        print 'Sending banner...'
+                        s.send('RETURNBANNER')
+                        connectionStage = 1
+                elif connectionStage == 1:
+                    if data[0:26] == '-----BEGIN PUBLIC KEY-----':
+                        # TODO: Store fingerprint of this key for host verification
+                        print 'Received server public key. Importing...'
+                        serverPubkey = RSA.importKey(data)
+                        print 'Sending our public key...'
+                        print len(serverPubkey.encrypt(pubkey.exportKey(),32))
+                        s.send(serverPubkey.encrypt(pubkey.exportKey(), 32))
+                        connectionStage = 2
+                elif connectionStage == 2:
+                    data = privkey.decrypt(data)
+                    if data == pubkey.exportKey():
+                        print 'Server identity verified.'
+                        print 'Connection Established.'
+                        connectionStage = 3
+
+                else:
+                    print 'SERVER: ' + data
+                    # server acknowledging our disconnect
+                    if data == 'BYE':
+                        running = False
         elif i == stdin:
             command = stdin.readline().strip()
             if command == 'exit':
--- server.py
+++ server.py
@@ -2,36 +2,79 @@ import socket
 import threading
 import sys
 import select
+from os.path import isfile
+from Crypto.PublicKey import RSA
 
 class Client(threading.Thread):
-    def __init__(self,(client,address)):
+    def __init__(self,(client,address), privkey, pubkey):
         threading.Thread.__init__(self)
         self.clientSocket = client
         self.address = address
+        self.privkey = privkey
+        self.pubkey = pubkey
     def run(self):
         running = True
+        connectionStage = 0
         while running:
-            data = self.clientSocket.recv(1000).strip()
+            data = self.clientSocket.recv(2048).strip()
             if data:
-                # echo back certain commands
-                if data == 'HELLO':
-                    self.send(data)
-                elif data == 'BYE':
-                    self.send(data)
-                # eval anything else
+                # connection initialization
+                if connectionStage == 0:
+                    if data == 'RETURNBANNER':
+                        connectionStage = 1
+                        self.send(self.pubkey.exportKey())
+                elif connectionStage == 1:
+                    data = privkey.decrypt(data)
+                    clientPubkey = RSA.importKey(data)
+                    self.send(clientPubkey.encrypt(data, 32))
+                    connectionStage = 3 # TODO CHANGE THIS TO 2
+                elif connectionStage == 2:
+                    # TODO
+                    pass
                 else:
-                    try:
-                        self.clientSocket.send(str(eval(data)))
-                    except:
-                        self.clientSocket.send('Failed to evaluate: ' + data)
+                    # echo back certain commands
+                    if data == 'HELLO':
+                        self.send(data)
+                    elif data == 'BYE':
+                        self.send(data)
+                        running = False
+                    # eval anything else
+                    else:
+                        try:
+                            self.clientSocket.send(str(eval(data)))
+                        except:
+                            self.clientSocket.send('Failed to evaluate: ' + data)
             else:
                 self.clientSocket.close()
                 running = False
     def send(self, msg):
         self.clientSocket.send(msg)
 
+# import/generate host RSA keys
+if isfile('server_hostkey') and isfile('server_hostkey.pub'):
+    privkeyString = file('server_hostkey', 'r').read()
+    pubkeyString = file('server_hostkey.pub', 'r').read()
+    privkey = RSA.importKey(privkeyString)
+    pubkey = RSA.importKey(pubkeyString)
+else:
+    print 'Generating new host RSA keys...'
+    privkey = RSA.generate(16384)
+    pubkey = privkey.publickey()
+    # write them to files for future use
+    privkeyString = privkey.exportKey()
+    pubkeyString = pubkey.exportKey()
+    privkeyFile = file('server_hostkey', 'w')
+    pubkeyFile = file('server_hostkey.pub', 'w')
+    privkeyFile.write(privkeyString)
+    pubkeyFile.write(pubkeyString)
+    privkeyFile.close()
+    pubkeyFile.close()
+
+bindaddress = 'localhost'
+port = 55516
+print 'Listening on ' + bindaddress + ':' + str(port)
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.bind(('localhost', 55516))
+serverSocket.bind((bindaddress, port))
 serverSocket.listen(5)
 clients = []
 input = [serverSocket, sys.stdin]
@@ -40,10 +83,10 @@ while running:
     inputready,outputready,exceptready = select.select(input, [], [])
     for s in inputready:
         if s == serverSocket:
-            c = Client(serverSocket.accept())
+            c = Client(serverSocket.accept(),privkey,pubkey)
             c.start()
             clients.append(c)
-            c.send('Hello client!')
+            c.send('BANNER')
         elif s == sys.stdin:
             command = sys.stdin.readline().strip()
             if command == 'exit':
